d0ad556b7545391d761a4b854e36a86f13da77c0
Phucdemon6868 <bigzonehp@gmail.com>
2025-09-28 11:58:35 +0700

    feat: Update icon svgs
    Refactors BuildingOfficeIcon, ClipboardDocumentListIcon, and UsersIcon to use updated SVG paths for improved appearance and consistency.

--- src/components/icons/BuildingOfficeIcon.tsx
+++ src/components/icons/BuildingOfficeIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BuildingOfficeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M8.25 21V3m8.25 18V3m-3 18v-6a.75.75 0 00-.75-.75h-4.5a.75.75 0 00-.75.75v6m-3-18v18" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h6M9 10.5h6M9 14.25h6M9 18h6" />
   </svg>
 );
 
