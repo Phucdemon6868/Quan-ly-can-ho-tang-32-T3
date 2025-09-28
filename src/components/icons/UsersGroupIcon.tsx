7a90334c1b85a05fefc5ca567fe68d5cf813d44d
Phucdemon6868 <bigzonehp@gmail.com>
2025-09-28 15:05:52 +0700

    feat: Update SVG icons for clarity
    Refactors the SVG paths for the Trash, Users, and UsersGroup icons. The new paths provide a more modern and visually distinct representation of the icons, improving the overall UI experience.

--- src/components/icons/UsersGroupIcon.tsx
+++ src/components/icons/UsersGroupIcon.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 const UsersGroupIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
@@ -13,7 +14,7 @@ const UsersGroupIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m-7.5-2.962a3.75 3.75 0 1 1 5.25 0m-5.25 0a3.75 3.75 0 0 0-5.25 0M3 13.5a9 9 0 0 1 18 0v2.25c0 .621-.504 1.125-1.125 1.125H4.125A1.125 1.125 0 0 1 3 15.75V13.5Z"
+      d="M17 20h5v-2a3 3 0 0 0-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 0 1 5.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 0 1 9.288 0M15 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM7 10a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
     />
   </svg>
 );
--- src/components/icons/UsersIcon.tsx
+++ src/components/icons/UsersIcon.tsx
@@ -1,8 +1,21 @@
+
 import React from 'react';
 
 const UsersIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m-7.5-2.962a3.75 3.75 0 015.25 0m-5.25 0a3.75 3.75 0 00-5.25 0M3 13.5a9 9 0 0118 0v2.25c0 .621-.504 1.125-1.125 1.125H4.125A1.125 1.125 0 013 15.75V13.5z" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.53-.292M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-4.663m-6.375-9.348a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Z"
+    />
   </svg>
 );
 
