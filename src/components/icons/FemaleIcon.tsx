b176d172929cf8ef19cbc6ab24238c58cd0ad91e
Phucdemon6868 <bigzonehp@gmail.com>
2025-09-28 15:10:12 +0700

    refactor(icons): Update SVG paths for better clarity
    Updates the SVG paths for UsersIcon and FemaleIcon to use a more standard and readable format. This improves the visual representation of these icons within the application.

--- src/components/icons/FemaleIcon.tsx
+++ src/components/icons/FemaleIcon.tsx
@@ -1,9 +1,11 @@
+
 import React from 'react';
 
 const FemaleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+    <path fillRule="evenodd" d="M12 5.25a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm-2.625 9a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-1.5 0v-3a.75.75 0 0 1 .75-.75Zm5.25 0a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-1.5 0v-3a.75.75 0 0 1 .75-.75ZM12 3.75a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM10.5 12.75a7.5 7.5 0 1 0 3 0h-3Z" clipRule="evenodd" />
   </svg>
 );
+
 
 export default FemaleIcon;
